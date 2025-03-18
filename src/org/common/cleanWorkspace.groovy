23e027b3bd4ad9dba030c21ae732b2fe10e125e9
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-18 11:08:12 +0530

    Update cleanWorkspace.groovy

--- src/org/common/cleanWorkspace.groovy
+++ src/org/common/cleanWorkspace.groovy
@@ -1,1 +1,15 @@
 
+package org
+
+class CleanWorkspace {
+    def steps
+
+    CleanWorkspace(steps) {
+        this.steps = steps
+    }
+
+    def cleanWorkspace() {
+        steps.echo "Cleaning workspace..."
+        steps.deleteDir()
+    }
+}
