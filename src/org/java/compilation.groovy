1f12861e299701cf138526c1f7902c7c33a16a03
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-18 11:01:39 +0530

    Update compilation.groovy

--- src/org/java/compilation.groovy
+++ src/org/java/compilation.groovy
@@ -1,1 +1,13 @@
+package org.java
 
+class Compilation {
+    def steps
+    Compilation(steps) {
+        this.steps = steps
+    }
+
+    def mavenCompile() {
+        steps.echo "Compiling code using Maven..."
+        steps.sh "mvn clean compile"
+    }
+}
