8a37447ab16741f52e64a85c32c1bd35a7dcdeca
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-18 11:06:35 +0530

    Update gitClone.groovy

--- src/org/common/gitClone.groovy
+++ src/org/common/gitClone.groovy
@@ -1,1 +1,19 @@
 
+package org
+
+class GitClone {
+    def steps
+
+    GitClone(steps) {
+        this.steps = steps
+    }
+
+    def gitClone(branch, repoUrl, credentialsId) {
+        steps.echo "Cloning repository from ${repoUrl} - branch: ${branch}"
+        steps.checkout([
+            $class: 'GitSCM',
+            branches: [[name: "refs/heads/${branch}"]],
+            userRemoteConfigs: [[url: repoUrl, credentialsId: credentialsId]]
+        ])
+    }
+}
